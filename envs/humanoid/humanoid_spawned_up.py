fix ot reward calculation

envs/humanoid/humanoid_spawned_up.py
@@ -10,6 +10,8 @@ from numpy.typing import NDArray
 
 from envs.humanoid.reward_helpers import *
 
+from constants import SEQ_DICT
+
 def mass_center(model, data):
     mass = np.expand_dims(model.body_mass, axis=1)
     xpos = data.xipos
@@ -25,11 +27,13 @@ DEFAULT_CAMERA_CONFIG = {
 
 class HumanoidEnvCustom(GymHumanoidEnv):
     DEMOS_DICT = {
-        "both_arms_out_goal_only_euclidean": ["create_demo/demos/both-arms-out_joint-state.npy"],
-        "both_arms_out_seq_euclidean": ["create_demo/demos/left-arm-out_joint-state.npy", "create_demo/demos/both-arms-out_joint-state.npy"],
-        "arms_up_then_down_seq_euclidean": ["create_demo/demos/left-arm-out_joint-state.npy", "create_demo/demos/both-arms-out_joint-state.npy", "create_demo/demos/right-arm-out_joint-state.npy"],
-        "arms_up_then_down_seq_stage_detector": ["create_demo/demos/left-arm-out_joint-state.npy", "create_demo/demos/both-arms-out_joint-state.npy", "create_demo/demos/right-arm-out_joint-state.npy"],
-        "arms_up_then_down_seq_avg": ["create_demo/demos/left-arm-out_joint-state.npy", "create_demo/demos/both-arms-out_joint-state.npy", "create_demo/demos/right-arm-out_joint-state.npy"],
+        "both_arms_out_goal_only_euclidean": SEQ_DICT["both_arms_out"],
+        "both_arms_out_seq_euclidean": SEQ_DICT["both_arms_out_with_intermediate"],
+        "both_arms_out_basic_r": SEQ_DICT["both_arms_out_with_intermediate"],
+        "arms_up_then_down_seq_euclidean": SEQ_DICT["arms_up_then_down"],
+        "arms_up_then_down_seq_stage_detector": SEQ_DICT["arms_up_then_down"],
+        "arms_up_then_down_seq_avg": SEQ_DICT["arms_up_then_down"],
+        "arms_up_then_down_basic_r": SEQ_DICT["arms_up_then_down"],
     }
 
     def __init__(
@@ -107,8 +111,10 @@ class HumanoidEnvCustom(GymHumanoidEnv):
 
         self._ref_joint_states = np.array([])
 
-        if "_goal_only_" in reward_type or "_seq_" in reward_type:
+        if reward_type in self.DEMOS_DICT:
             self._load_reference_joint_states(self.DEMOS_DICT[reward_type])
+        else:
+            logger.info(f"Warning: {reward_type} is not in the DEMOS_DICT. No reference joint states loaded.")
 
         # Spawned the humanoid not so high
         self.init_qpos[2] = 1.3
@@ -578,8 +584,8 @@ def reward_seq_stage_detector(data, **kwargs):
     curr_stage_unweighted_reward  = unweighted_reward_for_each_ref[curr_stage]
     
     # If the reward for the current stage is large enough, move to the next stage
-    if curr_stage_unweighted_reward > 0.85:
-        new_stage = np.min(curr_stage + 1, num_ref_joint_states - 1)
+    if curr_stage_unweighted_reward > 0.4:
+        new_stage = min(curr_stage + 1, num_ref_joint_states - 1)
     else:
         new_stage = curr_stage
 
@@ -638,6 +644,36 @@ def reward_seq_avg(data, **kwargs):
     return reward, terms_to_plot
 
 
+def reward_only_basic_r(data, **kwargs):
+    """Only provide basic reward to remain standing and control cost
+    """
+    original_mujoco_reward, _ = reward_original(data, **kwargs)
+
+    basic_standing_reward, terms_to_plot = basic_remain_standing_rewards(data, 
+                                                            upward_reward_w=1, 
+                                                            ctrl_cost_w=1, 
+                                                            **kwargs)
+    
+    reward =  basic_standing_reward
+
+    # Still calculating the pose matching reward (to individual poses) to show in the terms_to_plot
+    assert "ref_joint_states" in kwargs, "ref_joint_states must be passed in as part of the kwargs"
+    ref_joint_states = kwargs.get('ref_joint_states', None)
+
+    num_ref_joint_states = ref_joint_states.shape[0]
+
+    curr_qpos = data.qpos.flat.copy()[2:]
+
+    unweighted_reward_for_each_ref = np.exp(-np.linalg.norm(curr_qpos - ref_joint_states, axis=1))
+
+    terms_to_plot["pose_r_l"] = str([f"{unweighted_reward_for_each_ref[i]:.2f}" for i in range(num_ref_joint_states)])
+    terms_to_plot["r"] = f"{reward:.2f}"
+    terms_to_plot["og_r"] = f"{original_mujoco_reward:.2f}"
+    terms_to_plot["steps"] = kwargs.get("num_steps", 0)
+    
+    return reward, terms_to_plot
+
+
 REWARD_FN_MAPPING = dict(
         original = reward_original,
         simple_remain_standing = reward_simple_remain_standing,
@@ -648,8 +684,10 @@ REWARD_FN_MAPPING = dict(
         splitting = reward_splitting,
         both_arms_out_goal_only_euclidean = reward_both_arms_out_goal_only_euclidean,
         both_arms_out_seq_euclidean = reward_seq_euclidean,
+        both_arms_out_basic_r = reward_only_basic_r,
         arms_up_then_down_seq_euclidean = reward_seq_euclidean,
         arms_up_then_down_seq_stage_detector = reward_seq_stage_detector,
         arms_up_then_down_seq_avg = reward_seq_avg,
+        arms_up_then_down_basic_r = reward_only_basic_r,
     )
     

sbx/common/callbacks.py
@@ -65,17 +65,18 @@ class OTRewardCallback(BaseCallback):
 
         rewards = np.stack(ot_reward_list, axis=1)  # size: (train_freq, n_envs)
 
+        # Add the optimal transport reward to exisiting rewards
         if replay_buffer_pos - env_episode_timesteps >= 0:
             self.model.replay_buffer.rewards[
                 replay_buffer_pos - env_episode_timesteps : replay_buffer_pos, :
-            ] = rewards[:, :]
+            ] += rewards[:, :]
         else:
             # Split reward assignment (circular buffer)
             self.model.replay_buffer.rewards[
                 -(env_episode_timesteps - replay_buffer_pos) :, :
-            ] = rewards[: env_episode_timesteps - replay_buffer_pos, :]
+            ] += rewards[: env_episode_timesteps - replay_buffer_pos, :]
 
-            self.model.replay_buffer.rewards[:replay_buffer_pos, :] = rewards[
+            self.model.replay_buffer.rewards[:replay_buffer_pos, :] += rewards[
                 env_episode_timesteps - replay_buffer_pos :, :
             ]
 
@@ -163,11 +164,11 @@ class VideoRecorderCallback(BaseCallback):
 
     def _on_step(self) -> bool:
         if self.n_calls % self._render_freq == 0:
+            raw_screens = []
             screens = []
             states = []
             infos = []
             rewards = []
-            video_writer = imageio.get_writer(os.path.join(self._rollout_save_path, f"raw_video_{self.num_timesteps}.mp4"), fps=30)
 
             def grab_screens(_locals: Dict[str, Any], _globals: Dict[str, Any]) -> None:
                 """
@@ -183,11 +184,8 @@ class VideoRecorderCallback(BaseCallback):
 
                 image_int = np.uint8(screen)
 
-                video_writer.append_data(image_int)
-
-                pil_image = Image.fromarray(image_int)
-
-                screens.append(pil_image)
+                raw_screens.append(Image.fromarray(image_int))
+                screens.append(Image.fromarray(image_int))  # The frames here will get plotted with info later
                 infos.append(_locals.get('info', {}))
                 states.append(_locals["observations"])
                 rewards.append(_locals["rewards"])
@@ -200,6 +198,9 @@ class VideoRecorderCallback(BaseCallback):
                 deterministic=self._deterministic,
             )
 
+            # Save the raw_screens locally
+            imageio.mimsave(os.path.join(self._rollout_save_path, f"{self.num_timesteps}_rollouts.gif"), raw_screens, duration=1/30, loop=0)
+
             # Originally, states is a list of np.arrays size (1, env_obs_size)
             #   We want to concatenate them to get a single np.array size (n_timesteps, env_obs_size)
             states = np.concatenate(states)
@@ -210,6 +211,10 @@ class VideoRecorderCallback(BaseCallback):
 
                 self.logger.record("rollout/avg_ot_reward", 
                                 np.mean(ot_reward), 
+                                exclude=("stdout", "log", "json", "csv"))
+                
+                self.logger.record("rollout/avg_total_reward", 
+                                np.mean(ot_reward + rewards), 
                                 exclude=("stdout", "log", "json", "csv"))
 
                 # Add the ot_reward to the infos so that we can plot it

vlm_reward/utils/optimal_transport.py
@@ -38,11 +38,11 @@ def compute_ot_reward(obs: np.ndarray, ref: np.ndarray, cost_fn) -> np.ndarray:
     ref_weight = np.ones(ref.shape[0]) / ref.shape[0]
     T = ot.sinkhorn(obs_weight, ref_weight, cost_matrix, reg=0.01, log=False)  # size: (train_freq, ref_seq_len)
 
-    # Calculate the OT reward for each timestep
+    # Calculate the OT cost for each timestep
     #   sum by row of (cost matrix * OT plan)
-    ot_reward = np.sum(cost_matrix * T, axis=1)  # size: (train_freq,)
+    ot_cost = np.sum(cost_matrix * T, axis=1)  # size: (train_freq,)
 
-    return ot_reward
+    return - ot_cost
 
 def cosine_distance(x, y):
     distance = np.dot(x, y.T) / np.linalg.norm(x, axis=1, keepdims=True) / np.linalg.norm(y.T, axis=0, keepdims=True) # Transpose B to match dimensions