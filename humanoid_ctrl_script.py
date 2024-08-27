b5ef9527d20315c3b0e390243dda80e805601ebb
Yuki Wang <yukiluna1999@gmail.com>
2024-08-27 11:00:57 -0400

    comment out debug code in humanoid_ctrl_script.py

--- humanoid_ctrl_script.py
+++ humanoid_ctrl_script.py
@@ -92,41 +92,41 @@ if args.debug:
         video_writer.append_data(np.uint8(env.render()))
 
     # TODO: Remove Below. Temporary debugging to check the reward function
-    for i in range(50):
-        # Test the environment with random action (force-controlled)
-        o, _, _, _, info = env.step(np.random.uniform(-0.4, 0.4, (17,)))
-        frame = env.render()
-        image = Image.fromarray(frame)
-        plot_info_on_frame(image, info)
-        video_writer.append_data(np.uint8(image))
+    # for i in range(50):
+    #     # Test the environment with random action (force-controlled)
+    #     o, _, _, _, info = env.step(np.random.uniform(-0.4, 0.4, (17,)))
+    #     frame = env.render()
+    #     image = Image.fromarray(frame)
+    #     plot_info_on_frame(image, info)
+    #     video_writer.append_data(np.uint8(image))
 
-        obs = env.unwrapped.get_obs()
-        print(f"{i}:")
-        for j in range(len(obs[:22])):
-            print(f"  qpos: {j+2}: {obs[:22][j]:.2f}")
+    #     obs = env.unwrapped.get_obs()
+    #     print(f"{i}:")
+    #     for j in range(len(obs[:22])):
+    #         print(f"  qpos: {j+2}: {obs[:22][j]:.2f}")
 
-    plist = ["both-arms-out", "right-arm-out", "left-arm-out"]
-    for i in range(3):
-        joint_config = pose_config_dict[plist[i]]
+    # plist = ["both-arms-out", "right-arm-out", "left-arm-out"]
+    # for i in range(3):
+    #     joint_config = pose_config_dict[plist[i]]
 
-        new_qpos = copy.deepcopy(init_qpos)
-        for idx in joint_config.keys():
-            new_qpos[int(idx)] = joint_config[int(idx)] # z-coordinate of torso
+    #     new_qpos = copy.deepcopy(init_qpos)
+    #     for idx in joint_config.keys():
+    #         new_qpos[int(idx)] = joint_config[int(idx)] # z-coordinate of torso
 
-        # Set the humanoid joint state to the specified ones, velocity stays at 0
-        env.unwrapped.set_state(qpos=new_qpos, qvel=np.zeros((23,)))
+    #     # Set the humanoid joint state to the specified ones, velocity stays at 0
+    #     env.unwrapped.set_state(qpos=new_qpos, qvel=np.zeros((23,)))
 
-        ref = np.load("create_demo/demos/both-arms-out_joint-state.npy")
-        obs = env.unwrapped.get_obs()
-        print(f"{i}: {np.exp(-np.linalg.norm(obs[:22] - ref[0]))}")
+    #     ref = np.load("create_demo/demos/both-arms-out_joint-state.npy")
+    #     obs = env.unwrapped.get_obs()
+    #     print(f"{i}: {np.exp(-np.linalg.norm(obs[:22] - ref[0]))}")
 
-        for j in range(5):
-            # Test the environment with random action (force-controlled)
-            o, _, _, _, info = env.step(np.zeros((17,)))
-            frame = env.render()
-            image = Image.fromarray(frame)
-            plot_info_on_frame(image, info)
-            video_writer.append_data(np.uint8(image))
+    #     for j in range(5):
+    #         # Test the environment with random action (force-controlled)
+    #         o, _, _, _, info = env.step(np.zeros((17,)))
+    #         frame = env.render()
+    #         image = Image.fromarray(frame)
+    #         plot_info_on_frame(image, info)
+    #         video_writer.append_data(np.uint8(image))
     # TODO: Remove Above. Temporary debugging to check the reward function
                         
     video_writer.close()
